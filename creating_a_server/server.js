d53e62380b0c858c4f75ca0d0a92207a8e6e5495
elyse502 <elyseniyibizi502@gmail.com>
2026-04-24 10:04:42 +0200

    feat: enhance HTTP server with request logging and HTML response
        - Add request URL and method logging for debugging
        - Set response content type to text/html for browser rendering
        - Write HTML response with multiple headings
        - Include link tag placeholder in response head section
        - Use res.end() to complete response and send to client
        - Comment out plain text content type for future reference
        - Improve server response for better client interaction

--- creating_a_server/server.js
+++ creating_a_server/server.js
@@ -1,7 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Request made.");
+  //   console.log("Request made.");
+  //   console.log(req);
+  console.log(req.url, req.method);
+
+  // Set header content type
+  // res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Type", "text/html");
+
+  res.write('<head><link rel="stylesheet" href="#"/></head>');
+  res.write("<h1>Hello, Devs!</h1>");
+  res.write("<h1>Hello again, Devs!</h1>");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {
