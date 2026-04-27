966b28c913ec5c43fd0e878e7ee3ff6eb5e066e6
elyse502 <elyseniyibizi502@gmail.com>
2026-04-27 13:35:16 +0200

    feat: integrate EJS template engine for dynamic views
        - Install EJS template engine as dependency
        - Configure Express to use EJS as view engine with app.set()
        - Add default views directory (configurable with app.set("views"))
        - Convert static HTML files to EJS templates (index.ejs, about.ejs, 404.ejs)
        - Create new create.ejs template for blog creation form
        - Replace res.sendFile() with res.render() for template rendering
        - Remove redirect route and update to blog creation route
        - Delete old HTML files and view_0 directory
        - Preserve 404 error handling with EJS template rendering
        - Improve maintainability with reusable layout potential

--- express/app.js
+++ express/app.js
@@ -3,31 +3,27 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+// app.set("views", "myviews"); // default is views folder, if we want to change it we can use this line of code
+
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  //   res.send("<p>Home page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  //   res.send("<p>About page</p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
 // redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
-// 404 page - must be at the end of all routes
-/**
- * This should be the last route because it will match all the routes
- * that are not defined above and it will send the 404 page, in case we
- * have placed it in between where we have other routes above they won't
- * get reached because it's synchronous.
- */
+// 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
