4d33e61cd1f13e2bff0696d6082da282b035b2a6
elyse502 <elyseniyibizi502@gmail.com>
2026-05-04 15:27:43 +0200

    feat: add route to retrieve all blogs from database
        - Create GET route "/all-blogs" for fetching all blog documents
        - Use Blog.find() without parameters to retrieve all records
        - Send array of blog documents as JSON response
        - Add error handling for database query operations
        - Add logging for database errors in console
        - Prepare foundation for displaying blogs in application views

--- express/app.js
+++ express/app.js
@@ -45,6 +45,16 @@ app.get("/add-blog", (req, res) => {
     });
 });
 
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get("/", (req, res) => {
   const blogs = [
     {
