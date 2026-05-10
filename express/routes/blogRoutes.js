dd738f76002fd116421bb7644b59ac7410a8e700
elyse502 <elyseniyibizi502@gmail.com>
2026-05-10 10:11:35 +0200

    refactor: simplify blog route paths with base route mounting
        - Mount blogRoutes under "/blogs" base path in main app
        - Remove redundant "/blogs" prefix from individual route definitions
        - Update GET "/" route for listing all blogs
        - Update POST "/" route for creating new blogs
        - Update GET "/create" route for new blog form
        - Update GET "/:id" route for single blog details
        - Update DELETE "/:id" route for blog deletion
        - Improve route organization and reduce duplication
        - Maintain all functionality with cleaner URL structure

--- express/routes/blogRoutes.js
+++ express/routes/blogRoutes.js
@@ -3,7 +3,7 @@ const Blog = require("../models/blog");
 
 const router = express.Router();
 
-router.get("/blogs", (req, res) => {
+router.get("/", (req, res) => {
   Blog.find()
     .sort({ createdAt: -1 })
     .then((result) => {
@@ -14,7 +14,7 @@ router.get("/blogs", (req, res) => {
     });
 });
 
-router.post("/blogs", (req, res) => {
+router.post("/", (req, res) => {
   const blog = new Blog(req.body);
 
   blog
@@ -27,11 +27,11 @@ router.post("/blogs", (req, res) => {
     });
 });
 
-router.get("/blogs/create", (req, res) => {
+router.get("/create", (req, res) => {
   res.render("create", { title: "Create a new Blog" });
 });
 
-router.get("/blogs/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   const id = req.params.id;
   Blog.findById(id)
     .then((result) => {
@@ -42,7 +42,7 @@ router.get("/blogs/:id", (req, res) => {
     });
 });
 
-router.delete("/blogs/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
   const id = req.params.id;
 
   Blog.findByIdAndDelete(id)
